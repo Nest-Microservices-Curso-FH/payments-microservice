11c99082c05317c27c1cab03643968a5c1954cee
tomasrpita <tomasrpita@gmail.com>
2024-06-04 06:50:21 +0200

    feat: add DTO for payment session and update service to handle dynamic data

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,30 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { envs } from 'src/config';
 import Stripe from 'stripe';
+import { PaymentsSessionDto } from './dto/payment-session.dto';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
 
-  async createPaymentSession() {
+  async createPaymentSession(paymentSessionDto: PaymentsSessionDto) {
+    const { currency, items } = paymentSessionDto;
+
+    const line_items = items.map((item) => {
+      return {
+        price_data: {
+          currency: currency,
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: Math.round(item.price * 100), // 20 - 2000 / 100 * 20.00
+        },
+        quantity: item.quantity,
+      };
+    });
+
     const session = await this.stripe.checkout.sessions.create({
       // Colocar aquí el ID del pedido
       payment_intent_data: {
         metadata: {},
       },
 
-      line_items: [
-        {
-          price_data: {
-            currency: 'eur',
-            product_data: {
-              name: 'T-Shirt',
-            },
-            unit_amount: 2000, // 20 - 2000 / 100 * 20.00
-          },
-          quantity: 2,
-        },
-      ],
+      line_items: line_items,
       mode: 'payment',
       success_url: 'http://localhost:3003/payments/success',
       cancel_url: 'http://localhost:3003/payments/cancel',
