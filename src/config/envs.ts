4630c09f8f41bde91cc77c8d2cf479ca643235f0
tomasrpita <tomasrpita@gmail.com>
2024-06-06 06:20:59 +0200

    feat: transform payments-ms to hybrid microservice with NATS and HTTP support

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -8,6 +8,8 @@ interface EnvVars {
   STRIPE_ENDPOINT_SECRET: string;
   STRIPE_SUCCESS_URL: string;
   STRIPE_CANCEL_URL: string;
+  NATS_SERVERS:  string[];
+
 }
 
 const envsSchema = joi
@@ -17,12 +19,13 @@ const envsSchema = joi
     STRIPE_ENDPOINT_SECRET: joi.string().required(),
     STRIPE_SUCCESS_URL:joi.string().required(),
     STRIPE_CANCEL_URL:joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
 const { error, value } = envsSchema.validate({
   ...process.env,
-  //   NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+    NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
 });
 
 if (error) {
@@ -36,6 +39,8 @@ export const envs = {
   stripeSecret: envVars.STRIPE_SECRET,
   stripeSeccessUrl: envVars.STRIPE_SUCCESS_URL,
   stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
-  stripeEndpointSecret: envVars.STRIPE_ENDPOINT_SECRET
+  stripeEndpointSecret: envVars.STRIPE_ENDPOINT_SECRET,
+  natsServers: envVars.NATS_SERVERS
+
 
 };
